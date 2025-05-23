24beb7c402a567ba4aff5fbdec637bc43d777df1
PKSLZ <peneloyerock@gmail.com>
2025-05-23 10:48:38 -0300

    Criação das classes de Book

---
+++ src/main/java/br/uema/project/project/api/BookController.java
@@ -0,0 +1,14 @@
+package br.uema.project.project.api;
+
+import br.uema.project.project.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    private BookService service;
+}
